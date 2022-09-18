c8bd2fb83ab8aeadd9f7b7998dea49963fb124fd
EdmilsonDomingues <64106312+EdmilsonDomingues@users.noreply.github.com>
2022-09-18 14:31:06 -0300

    Alteração principalmente dos nomes de métodos

--- main.py
+++ main.py
@@ -13,24 +13,24 @@ metodo_utilizado = 1
 # método 2: A* com heurística mais precisa que conseguirem
 
 puzzle = Puzzle8(configuracao_inicial, configuracao_final, metodo_utilizado)
-puzzle.set_Abertos(configuracao_inicial)
-puzzle.set_Fechados([])
+puzzle.set_nodos_abertos(configuracao_inicial)
+puzzle.set_nodos_fechados([])
 continua = True
-while (continua and puzzle.tem_Abertos()):
-    nodo_da_vez = puzzle.get_nodo_da_vez()
+while (continua and puzzle.tem_abertos()):
+    nodo_da_vez = puzzle.menor_custo_abertos()
     if (puzzle.eh_nodo_objetivo(nodo_da_vez)):
         print("Caminho Final: ", puzzle.resultado())
         print("Tamanho do caminho: ", tamanho_do_caminho_final())
-        print("Total de Nodos Abertos: ", puzzle.get_total_nodos_Abertos())
-        print("Nodos Abertos: ", puzzle.get_Abertos())
-        print("Total de Nodos Fechados: ", puzzle.get_total_nodos_Fechados())
-        print("Nodos Fechados: ", puzzle.get_Fechados())
+        print("Total de Nodos Abertos: ", puzzle.get_total_nodos_abertos())
+        print("Nodos Abertos: ", puzzle.get_nodos_abertos())
+        print("Total de Nodos Fechados: ", puzzle.get_total_nodos_fechados())
+        print("Nodos Fechados: ", puzzle.get_nodos_fechados())
         continua = False
     else:
-        filhos = puzzle.gera_filhos(nodo_da_vez)
+        filhos = puzzle.gera_nodos_filhos(nodo_da_vez)
         for i in length(filhos):
-            if (not puzzle.esta_em_Abertos(filhos[i]) and not puzzle.esta_em_Fechados(filhos[i])):
-                puzzle.atribui_custos_ao_nodo(filhos[i])
+            if (not puzzle.esta_em_nodos_abertos(filhos[i]) and not puzzle.esta_em_nodos_fechados(filhos[i])):
+                puzzle.atribui_custos_ao_nodo(filhos[i])**
                 puzzle.coloca_em_Abertos(filhos[i])
             else:
                 if (puzzle.esta_em_Abertos(filhos[i])):
@@ -44,8 +44,3 @@ if continua == True:
     print("Falha")
 del puzzle
 gc.collect()
-
-
-                  
-
-
--- puzzle.py
+++ puzzle.py
@@ -1,13 +1,17 @@
 
 class Puzzle8:
 
-    def __init__(self, estado_inicial, estado_final, metodo_utilizado=1):
+    def __init__(self, estado_inicial, estado_final, metodo_utilizado):
         self.estado_final = estado_final
         self.estado_inicial = estado_inicial
         self.nodos_abertos = []
         self.nodos_fechados = []
         self.proximo_nodo = []
+        self.nodo_da_vez = []
         self.metodo_utilizado = metodo_utilizado
+        # método 0: custo uniforme (sem heurística)
+        # método 1: A* com heurística simples
+        # método 2: A* com heurística mais precisa que conseguirem
 
     def get_estado_final(self):
         return self.estado_final
@@ -27,7 +31,6 @@ class Puzzle8:
     def set_nodos_abertos(self, nodos_abertos):
         self.nodos_abertos = nodos_abertos
 
-
     def get_nodos_fechados(self):
         return self.nodos_fechados
 
@@ -35,9 +38,10 @@ class Puzzle8:
         self.nodos_fechados = nodos_fechados
 
     def tem_abertos(self):
-        if len(self.nodos_abertos) > 0:
-            return True
-        return False
+        # if len(self.nodos_abertos) > 0:
+        #   return True
+        #return False
+        return len(self.nodos_abertos > 0)
 
     def get_total_nodos_abertos(self):
         return len(self.get_nodos_abertos())
@@ -46,9 +50,11 @@ class Puzzle8:
         return len(self.get_nodos_fechados())
 
     def menor_custo_abertos(self):
-        nodos_abertos_em_ordem = self.ordena_nodos_abertos()
-        self.set_nodos_abertos(nodos_abertos_em_ordem)
-        return nodos_abertos_em_ordem[0]
+        # nodos_abertos_em_ordem = self.ordena_nodos_abertos() # Teoricamente já está ordenado.
+        # self.set_nodos_abertos(nodos_abertos_em_ordem)
+        # return nodos_abertos_em_ordem[0]
+        self.nodo_da_vez = nodos_abertos[0][len(nodos_abertos[0] -1)]
+        return self.nodo_da_vez
 
     def ordena_nodos_abertos(self):
         nodos_abertos_em_ordem = self.get_nodos_abertos()
@@ -56,13 +62,48 @@ class Puzzle8:
         return nodos_abertos_em_ordem
 
     def eh_nodo_objetivo(self, nodo_da_vez):
-        nodo_objetivo = self.get_estado_final()
-        if nodo_da_vez == nodo_objetivo:
+        # nodo_objetivo = self.get_estado_final()
+        # if nodo_da_vez == nodo_objetivo:
+        #    return True
+        # return False
+        return nodo_da_vez == self.get_estado_final() # Existe comparação de listas, assim?
+
+    def resultado(self):
+        # pass
+        return self.nodo_da_vez
+    
+    def tamanho_do_caminho_final(self):
+        return self.nodo_da_vez[1] # Criei este método.
+    
+
+    def esta_em_nodos_abertos(self, nodos_filhos):
+        if nodos_filhos in self.get_nodos_abertos():
             return True
         return False
 
-    def resultado(self):
-        pass
+    def esta_em_nodos_abertos(self, nodo_filho): # reescrevendo o método acima
+        if len(self.nodos_abertos) == 0:
+            return False
+        for i in len(self.nodos_abertos):
+            if nodo_filho in self.nodos_abertos[i][len(self.nodos_abertos[i]) -1] # "in" funciona?
+                return True
+        return False
+
+    def esta_em_nodos_fechados(self, nodos_filhos):
+        if nodos_filhos in self.get_nodos_fechados:
+            return True
+        return False
+    
+    def esta_em_nodos_fechados(self, nodo_filho): # reescrevendo o método acima
+        if len(self.nodos_fechados) == 0:
+            return False
+        for i in len(self.nodos_fechados):
+            if nodo_filho in self.nodos_fechados[i][len(self.nodos_fechados[i]) -1] # "in" funciona?
+                return True
+        return False
+    
+    def atribui_custos_ao_nodo(nodo_filho):
+        
 
     def gera_nodos_filhos(self, nodo_da_vez):
         pai = nodo_da_vez
@@ -331,12 +372,3 @@ class Puzzle8:
         ]
         return nodos_filhos[ordem]
 
-    def esta_em_nodos_abertos(self, nodos_filhos):
-        if nodos_filhos in self.get_nodos_abertos():
-            return True
-        return False
-
-    def esta_em_nodos_fechados(self, nodos_filhos):
-        if nodos_filhos in self.get_nodos_fechados:
-            return True
-        return False
